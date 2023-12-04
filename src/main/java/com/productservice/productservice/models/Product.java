371e394d55a5e6f42484a7653bc7a06c83c6a1db
Pavan Kumar <pavanbk927@gmail.com>
2023-12-04 11:46:27 +0530

    DB configuration & UUID

--- src/main/java/com/productservice/productservice/models/Product.java
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -1,15 +1,19 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     public String title;
-    public String desc;
+    public String description;
     public Long price;
     public String image;
+    @ManyToOne
     public Category category;
 
 }
